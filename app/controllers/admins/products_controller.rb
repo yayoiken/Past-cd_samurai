3bb4ca9eb7569c729149a37eacb7a0d65ef1b079
om-nishimoto-ryoga <nishimoto.ryoga.infratop@gmail.com>
2019-06-22 12:20:34 +0900

    admin_login

---
+++ app/controllers/admins/products_controller.rb
@@ -0,0 +1,4 @@
+class Admin::ProductsController < Admin::ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root 'home#top'
+  namespace :admin do
+   resources :products # => /admin/products etc
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

88a062649a116063290a76dfaa858195fdb70f4e
unknown <hisohiso555>
2019-06-22 18:44:48 +0900

    clients_controller

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,22 @@
+class ClientsController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  	
+  end
+  
+
+  def destroy
+  	
+  end
+
+  def withdrawal
+  end
+
+  def ordered
+  end
+end

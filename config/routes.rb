c6b129fda72879f269ab747b846cee1aa0ac83fc
unknown <hisohiso555>
2019-06-22 17:36:55 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   namespace :admin do
    resources :products # => /admin/products etc
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

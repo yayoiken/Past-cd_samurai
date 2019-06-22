e34c0d74af4eb086fd060efc5f67385cf6bd26b7
unknown <hisohiso555>
2019-06-22 17:51:08 +0900

    update

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,21 @@
+class ClientsController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  	
+  end
+
+  def destroy
+  	
+  end
+
+  def withdrawal
+  end
+
+  def ordered
+  end
+end

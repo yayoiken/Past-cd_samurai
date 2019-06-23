d52c57365753d0ca87b6dc5ae6476db25abba3f8
om-nishimoto-ryoga <nishimoto.ryoga.infratop@gmail.com>
2019-06-23 19:00:23 +0900

    enumできた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
 
+protected
+ def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :gender])
+ end
 end

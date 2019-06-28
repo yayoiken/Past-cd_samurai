4661938fe51363a76518f302d550437c91545c20
maedayuya <maeda.yuya.infratop@gmail.com>
2019-06-28 06:22:40 +0000

    gem cocoon

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	attachment :product_image
+	has_many :product_discs
+	accepts_nested_attributes_for :product_discs, allow_destroy: true
 end
--- app/models/product_disc.rb
+++ app/models/product_disc.rb
@@ -1,2 +1,6 @@
 class ProductDisc < ApplicationRecord
+	belongs_to :product
+	has_many :product_disc_songs
+	accepts_nested_attributes_for :product_disc_songs, allow_destroy: true
+
 end
